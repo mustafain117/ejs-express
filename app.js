ac04ce80d39ceb15f6ffb65fe163028bd4322f9c
Mustafain Ali Khan <mustafain.khan@mail.mcgill.ca>
2020-03-17 23:27:11 -0400

    work route added, stylesheet linked

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const bodyParser = require('body-parser');
 const app = express();
 
 var items = ["eat","sleep","repeat"];
+var workItems = [];
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }))
+app.use(express.static("public"));
 
 app.get('/', function(req,res){
     let today = new Date();
@@ -21,13 +24,23 @@ app.get('/', function(req,res){
     let day = today.toLocaleDateString("en-US", options);
 
    
-    res.render('list', {kindOfDay : day, newItems: items});
+    res.render('list', {listTitle : day, newItems: items});
 })
 
 app.post('/', function(req,res){
     let newItem = req.body.newItem;
-    items.push(newItem);
-    res.redirect('/');
+
+    if(req.body.list === 'Work List'){
+        workItems.push(newItem);
+        res.redirect('/work');
+    }else{
+        items.push(newItem);
+        res.redirect('/');
+    }
+})
+
+app.get('/work', function(req,res){
+    res.render('list', {listTitle: "Work List", newItems: workItems});
 })
 
 app.listen(3000, function(){
